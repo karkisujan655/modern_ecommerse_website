631ff82aafdb923f9c19e0a65ee6ca4ce9c65ce9
karkisujan655 <karkisujan655@gmail.com>
2023-05-27 23:06:13 +0545

    complete Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { TbDeviceTvOld } from "react-icons/tb";
+import { TbWorld } from "react-icons/tb";
+import { AiOutlineAliwangwang } from "react-icons/ai";
+import { IoPeopleOutline } from "react-icons/io5";
+import { BsCameraVideo } from "react-icons/bs";
 
 export default function Navbar() {
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
         <div className="col-md-8">
-          <div className="glass" id="Navbar">
-            <div className="d-flex justify-content-center ">
-              <div className="col-md-12 text-center ">
-                <div className="navv">
-                <a href="/" className="d-block">Home</a>
-                <a href="/" className="d-block">Gadgets</a>
-                <a href="/" className="d-block">Web Services</a>
-                <a href="/" className="d-block">About Us</a>
-              </div>
+          <div class="glass" id="Navbar">
+            <div class="d-flex justify-content-center">
+              <div class="navv">
+                <div class="row">
+                  <div class="col-md-6 text-center">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><AiFillHome /></span>  Home
+                    </a>
+                  </div>
+                  <div class="col-md-6 text-center">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><TbDeviceTvOld /></span>  Gadgets
+                    </a>
+                  </div>
+                  <div class="col-md-6 text-center py-5">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><TbWorld /></span>   Web Services
+                    </a>
+                  </div>
+                  <div class="col-md-6 text-center py-5">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><AiOutlineAliwangwang /></span> About Us
+                    </a>
+                  </div>
+                  <div class="col-md-6 text-center">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><IoPeopleOutline /></span>  Contact Us
+                    </a>
+                  </div>
+                  <div class="col-md-6 text-center">
+                    <a href="/" class="d-block">
+                    <span class="icon1"><BsCameraVideo /></span>  Videos
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
